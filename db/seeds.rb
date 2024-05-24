8bfb9908b931101cb25851086d3ba2253be511be
OmarMWarraich <earthling986@gmail.com>
2024-05-24 08:46:05 +0500

    feat: Model JsonWebToken, Devise, Authenticate User

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@
 #   end
 
 10.times do |i|
-  Post.create!(title: "Post #{i}", body: "This is the body of post #{i}")
+  Post.create!(title: "Post #{i+1}", body: "This is the body of post #{i+1}")
 end
