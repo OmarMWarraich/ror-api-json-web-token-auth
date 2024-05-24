895f9476b2d1ecfe3468dea712de23e250545a04
OmarMWarraich <earthling986@gmail.com>
2024-05-24 09:23:56 +0500

    feat: Devise Custom JWT Authentication Strategy

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::AuthenticationController < ApiController
-  skip_before_action :authenticate_token!
 
   def create
     user = User.find_by(email: params[:user][:email])
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :set_default_format
-  before_action :authenticate_token!
+  before_action :authenticate_user!
 
   private
 
@@ -9,16 +9,4 @@ class ApiController < ApplicationController
     request.format = :json
   end
 
-  def authenticate_token!
-    payload = JsonWebToken.decode(auth_token)
-    @current_user = User.find(payload["sub"])
-  rescue JWT::ExpiredSignature
-    render json: {errors: ["Auth token has expired"]}, status: :unauthorized
-  rescue JWT::DecodeError
-    render json: {errors: ["Invalid auth token"]}, status: :unauthorized
-  end
-
-  def auth_token
-    @auth_token ||= request.headers.fetch("Authorization", "").split(" ").last
-  end
 end
